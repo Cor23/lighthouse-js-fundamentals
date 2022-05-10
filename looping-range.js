8cd58c702a40f7158ec142297460ee814b31baa2
corey <corey.cy.young@gmail.com>
2022-05-10 20:28:20 +0000

    Bug fixing needed

--- looping-range.js
+++ looping-range.js
@@ -1,26 +1,24 @@
-let returnedArray = [];
+let array = [];
+range(-5, 2, 3);
 
 function range(start, end, step){
   let count = start;
 
-  for(let i = 0; i < end; i++){
+  for(let i = 0; i <= end; i++){
     if(count <= end && step <= 1){
+      array.push(count);
       count++;
-      returnedArray.push(count);
-    } else if(count <= end && step > 1){
-      returnedArray.push(count);
+    } else if(count < end){
+      array.push(count);
       count = count + step;
+    } else if(start === undefined || end === undefined || step === undefined){
+      array = [];
+    } else if(start > end){
+      array = [];
+    } else if(step <= 0){
+      array = [];
     }
   }
-
-  if(start === undefined || end === undefined || step === undefined){
-    returnedArray = [];
-  } else if(start > end){
-    returnedArray = [];
-  } else if(step <= 0){
-    returnedArray = [];
-  }
-
+  array.push(count);
 }
-range(0, 8, 2);
-console.log(returnedArray);
+console.log(array);
